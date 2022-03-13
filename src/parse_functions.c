ec898d2feb4af9ecea022c44cfa053258fa04fce
deykypinheiro <pinheiromikael96@gmail.com>
2022-03-13 11:23:35 -0300

    fix: remove functions extras

--- src/parse_functions.c
+++ src/parse_functions.c
@@ -6,44 +6,11 @@
 /*   By: demikael <pinheiromikael96@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 12:11:18 by demikael          #+#    #+#             */
-/*   Updated: 2022/03/13 10:53:45 by demikael         ###   ########.fr       */
+/*   Updated: 2022/03/13 11:21:48 by demikael         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-int	search_index_path(t_pipex *pipex)
-{
-	int	path_index;
-
-	path_index = 0;
-	while (pipex->envp[path_index] && \
-	!ft_strnstr(pipex->envp[path_index], "PATH", 5))
-		path_index++;
-	return (path_index);
-}
-
-char	*make_cmd(t_pipex *pipex, int offset_cmd)
-{
-	char	**cmd;
-	char	*aux;
-
-	cmd = ft_split(pipex->argv[offset_cmd], ' ');
-	aux = ft_strdup(cmd[0]);
-	free_ptr_ptr(cmd);
-	return (aux);
-}
-
-char	*make_path_comand(char *path, char **cmd)
-{
-	char	*path_front_slash;
-	char	*complet_path;
-
-	path_front_slash = ft_strjoin(path, "/");
-	complet_path = ft_strjoin(path_front_slash, cmd[0]);
-	free(path_front_slash);
-	return (complet_path);
-}
 
 char	**cmd_valid(char **paths, char **cmd)
 {
--- src/pipex.h
+++ src/pipex.h
@@ -6,7 +6,7 @@
 /*   By: demikael <pinheiromikael96@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 12:56:12 by demikael          #+#    #+#             */
-/*   Updated: 2022/03/13 11:00:11 by demikael         ###   ########.fr       */
+/*   Updated: 2022/03/13 11:23:12 by demikael         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,15 +39,9 @@ typedef struct s_pipex
 }	t_pipex;
 
 void	set_default_pipe(t_pipex *pipe, int argc, char **argv, char **envp);
-int		offset_first_comand(t_pipex *pipex);
-int		search_index_path(t_pipex *pipex);
-int		valid_comands(t_pipex *pipex, int offset_comand, int *count);
 char	**set_comands(char *comands, char *envp[]);
-int		exec_comands(t_pipex *pipex);
 void	free_ptr_ptr(char **ptr);
 void	ft_exit(t_pipex *pipex, char **cmd, int index);
-void	message_error(char *c_path, int *n_cmd, int offset);
-int		offset_first_comand(t_pipex *pipex);
 char	**cmd_valid(char **paths, char **cmd);
 int		run_comands(t_pipex *pipex, char **cmd, int index);
 void	reverse_space_treat_comands(char **comands);
